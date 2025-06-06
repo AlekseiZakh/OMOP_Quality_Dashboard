168be6ed2086cdbf9ee8ee5107684426ecd51160
Aleksei Zakharov <97632791+AlekseiZakh@users.noreply.github.com>
2025-06-06 23:41:07 +0100

    Create __init__.py

---
+++ app/database/__init__.py
@@ -0,0 +1,7 @@
+from .connection import OMOPDatabase, build_connection_string, get_database_connection
+
+__all__ = [
+    'OMOPDatabase',
+    'build_connection_string', 
+    'get_database_connection'
+]
